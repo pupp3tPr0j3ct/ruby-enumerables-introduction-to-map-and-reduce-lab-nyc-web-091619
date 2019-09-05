37d85064c02749164cc21433bf604eb864640e05
Jason Santana <jsantana006@gmail.com>
2019-09-05 18:15:20 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,92 @@
-# My Code here....
+def map_to_negativize(source_array)
+   i = 0 
+   new_array = []
+   
+   while i < source_array.length
+      new_array.push(source_array[i] * (-1))
+      i += 1
+    end
+    
+ return new_array
+ 
+end
+
+def map_to_no_change(source_array)
+  
+  return source_array
+  
+end
+
+def map_to_double(source_array)
+  i = 0 
+  new_array = []
+  
+  while i < source_array.length
+    new_array.push(source_array[i] * 2)
+    i += 1 
+  end
+  
+  return new_array
+  
+end
+
+def map_to_square(source_array)
+  i = 0 
+  new_array = []
+  
+  while i < source_array.length
+    new_array.push(source_array[i] ** 2)
+    i += 1
+  end
+  
+  return new_array
+  
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+  i = 0 
+  reduced_total = starting_point 
+  
+    while i < source_array.length
+      reduced_total += source_array[i]
+      i += 1 
+    end
+    
+  p reduced_total
+    
+end
+
+def reduce_to_all_true(source_array)
+  i = 0 
+  result = false
+  
+  while i < source_array.length
+    if !!source_array[i] != true
+      result = false
+    else
+      result = true
+    end
+    i += 1 
+  end
+  
+  return result
+  
+end
+
+def reduce_to_any_true(source_array)
+  i = 0 
+  result = true 
+  
+  while i < source_array.length
+    if !!source_array[i] == true
+      result = true
+    else
+      result = false
+    end
+    i += 1 
+  end
+  
+  return result
+  
+end
+
